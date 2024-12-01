15bef986e72fe97540e5dfe8c51ae3bad25f291b
Miguel Gagliardo <migag9@gmail.com>
2024-12-01 22:09:37 +0000

    Fixing per CF version

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,15 +1,15 @@
-resource "cloudflare_dns_record" "tracker-db-root" {
+resource "cloudflare_record" "tracker-db-root" {
   zone_id = local.cloudflare_zone_id
   name    = "@"
-  value   = aws_eip.ejb-webserver-eip.public_ip
+  content = aws_eip.ejb-webserver-eip.public_ip
   type    = "A"
   ttl     = 3600
 }
 
-resource "cloudflare_dns_record" "tracker-db-txt-zoho" {
+resource "cloudflare_record" "tracker-db-txt-zoho" {
   zone_id = local.cloudflare_zone_id
   name    = "@"
-  value   = "zoho-verification=zb17498197.zmverify.zoho.com"
+  content = "zoho-verification=zb17498197.zmverify.zoho.com"
   type    = "TXT"
   ttl     = 3600
 }
