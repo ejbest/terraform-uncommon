eb3c55ac83fc7600d9980a820c2f6902d227c746
Miguel Gagliardo <migag9@gmail.com>
2024-12-01 19:18:22 +0000

    Adding Zoho TXT record, bumping CF version

--- cloudflare.tf
+++ cloudflare.tf
@@ -5,3 +5,11 @@ resource "cloudflare_dns_record" "tracker-db-root" {
   type    = "A"
   ttl     = 3600
 }
+
+resource "cloudflare_dns_record" "tracker-db-txt-zoho" {
+  zone_id = local.cloudflare_zone_id
+  name    = "@"
+  value   = "zoho-verification=zb17498197.zmverify.zoho.com"
+  type    = "TXT"
+  ttl     = 3600
+}
